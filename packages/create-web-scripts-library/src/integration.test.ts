f65fdf83b6ab0fe809614d60c3530be560ccec45
Paul Marbach <pmarbach@spotify.com>
2019-07-01 17:19:23 -0400

    test(create-web-scripts-library): extend test timeout for safety

--- packages/create-web-scripts-library/src/integration.test.ts
+++ packages/create-web-scripts-library/src/integration.test.ts
@@ -36,7 +36,7 @@ describe('integration test', () => {
     // check that all the commands work
     [
       { cmd: 'lint' },
-      { cmd: 'test' },
+      { cmd: 'test', asyncTimeout: 30000 },
       { cmd: 'build', asyncTimeout: 60000 },
     ].forEach(
       ({
