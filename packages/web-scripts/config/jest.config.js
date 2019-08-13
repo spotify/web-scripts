8bcfc7fd6f114ffa001e437423741cf81bbee94d
Paul Marbach <pmarbach@spotify.com>
2019-08-13 16:53:56 -0400

    fix(web-scripts): ignore TypeScript declaration files from coverage
    fixes #45

--- packages/web-scripts/config/jest.config.js
+++ packages/web-scripts/config/jest.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   rootDir: path.join(process.cwd(), 'src'),
   coverageDirectory: path.join(process.cwd(), 'coverage'),
-  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts'],
   preset: 'ts-jest/presets/js-with-ts',
   globals: {
     'ts-jest': {
