8deb37ad0aaba1849d20aa4b7c91d0009bc4af9a
Paul Marbach <pmarbach@spotify.com>
2020-01-02 14:15:15 -0500

    build(postinstall): detect web-scripts for postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,9 @@
-yarn lerna run bootstrap
+#!/bin/bash
+web-scripts postinstall --help &> /dev/null
+
+if ! [ $? -eq 0 ]; then
+  echo "Bootstrapping web-scripts..."
+  yarn lerna run bootstrap
+fi
+
 web-scripts postinstall --threshold low
