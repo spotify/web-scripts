8ebf914440335d9488588dad61de66f6c9f47c1e
Kai Mallea <kai@spotify.com>
2021-07-16 13:30:23 -0400

    docs(release.sh): add explanation to release script

--- release.sh
+++ release.sh
@@ -1,3 +1,10 @@
+# This release script determines if a publish should happen using a dry run of semantic release to determine if the
+# commits contain publishable changes (feat, bug, breaking change). If so, lerna will be used to perform the actual
+# publish, however, lerna also has its own logic for determining whether to publish. Specifically, it will run git diff
+# in each package, excluding markdown files, tests, etc. So any attempt to force a version bump with an empty commit or
+# readme tweak will fail. To get around this, we pass the `force-publish` option to lerna, skipping the git diff for
+# changed packages. This allows the semantic release dry run check to be the deciding factor for publishing.
+
 # this message is logged by semantic-release when one of the commits found by web-scripts should trigger a release
 expected_release_message="The release type for the commit is"
 
