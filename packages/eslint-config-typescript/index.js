5ea256cc7a05927d94dc5bf1974fdb4e3ee94bc1
Kai Mallea <kai@spotify.com>
2022-03-02 15:25:42 -0500

    fix(eslint): specify parserOptions.project

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -16,6 +16,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: ['tsconfig.json', 'packages/web-scripts/tsconfig.json'],
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
